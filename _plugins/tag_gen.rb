14cdeac0e82c1dfffd15c4fb47927560d3815eaf
Joel Gillman <joeltgillman@gmail.com>
2013-11-10 18:49:51 -0800

    Add tag formatting and coloring

--- _plugins/tag_gen.rb
+++ _plugins/tag_gen.rb
@@ -1,12 +1,28 @@
 module Jekyll
 
-  module TagUrlFilter
-    def tag_url(input)
+  module TagFilter
+    require 'digest'
+
+    def tag_url( input )
       "/tag/#{ input.to_permalink }"
+    end
+
+    # Returns the same 'color' given the same input string
+    def tag_color_name( _input )
+      colors = [
+        "primary",
+        "success",
+        "info",
+        "warning",
+        "danger",
+      ]
+      hash        = Digest::MD5.hexdigest _input
+      color_index = hash.to_i(16) % colors.count
+      colors[ color_index ]
     end
   end
 
-  Liquid::Template.register_filter(Jekyll::TagUrlFilter)
+  Liquid::Template.register_filter(Jekyll::TagFilter)
 
 
   class TagIndex < Page
